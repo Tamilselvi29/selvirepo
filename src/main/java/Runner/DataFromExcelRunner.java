a9099453ab54f5b58526107823b4f769b0961506
tamilselvi29 <tamilselvivettri@gmail.com>
2019-03-29 11:29:45 -0400

    added new runner file datafromexcelrunner

---
+++ src/main/java/Runner/DataFromExcelRunner.java
@@ -0,0 +1,23 @@
+package Runner;
+
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "E:\\selvi\\NewJava\\CucumbetTestNew\\src\\main\\java\\Features\\datafromexcel.feature",
+		glue={"stepDefinition"},
+		format= {"pretty","html:test-output1","json:json_output/test.json"},
+		strict=true,
+		monochrome=true,
+		dryRun=false
+		
+		)
+
+public class DataFromExcelRunner {
+
+}
